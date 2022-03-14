d102b3d25aacd7eb8aa538097d3f75dcef44b6f6
bemo <dima.cvik@gmail.com>
2022-03-14 16:27:17 +0500

    в процессе

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/pages/home.dart';
-import 'package:todo_app/pages/mainScreen.dart';
+//import 'package:todo_app/pages/mainScreen.dart';
 
-void main() => runApp(MaterialApp(
-  theme: ThemeData(
-    colorScheme: ColorScheme.fromSwatch(
-      brightness: Brightness.light,
-      primarySwatch: Colors.deepOrange
-    ).copyWith(
-      secondary: Colors.yellow
-    )
-  ),
-  initialRoute: '/',
-  routes: {
-    '/': (context) => const MainScreen(),
-    '/todo': (context) => const Home(),
-  },
-));
+void main() async {
+
+  //инициализация файрбейс (СТРОГО ДО ВЫЗОВА runApp)
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MaterialApp(
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSwatch(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange
+      ).copyWith(
+          secondary: Colors.yellow
+      )
+    ),
+    home: const Home(),
+  ));
+
+  // runApp(MaterialApp(
+  //   theme: ThemeData(
+  //       colorScheme: ColorScheme.fromSwatch(
+  //           brightness: Brightness.light,
+  //           primarySwatch: Colors.deepOrange
+  //       ).copyWith(
+  //           secondary: Colors.yellow
+  //       )
+  //   ),
+  //   initialRoute: '/',
+  //   routes: {
+  //     '/': (context) => const MainScreen(),
+  //     '/todo': (context) => const Home(),
+  //   },
+  // ));
+}
+
+
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,64 +14,55 @@ class _HomeState extends State<Home> {
   String _emptyInfo = 'Тут пока ничего нет';
   final String _titleAppBar = 'Планировщик';
 
-  //ф-я асинхр. для подключения Файрбейс (ее помещаем в ИнитСтейт)
-  void initFirebase() async {
-    //инициалиация и подключение файрбейс
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
-  }
+  List<TextOverflow> _overTextList = [];
+  TextOverflow _overText = TextOverflow.ellipsis;
 
-  //проверка на отсутствие задач
-  void checkEmptyTodoList() {
-    if(todoList.isNotEmpty) {
-      _emptyInfo = '';
-    } else {
-      _emptyInfo = 'Тут пока ничего нет';
-    }
-  }
-
-  //удаление задачи
-  void deleteTodo(int index) {
-    setState(() {
-      todoList.removeAt(index);
-      checkEmptyTodoList();
-    });
-  }
-
-  final String _debugVar = '';
+  //var _debugVar;
 
   @override
   void initState() {
     super.initState();
-
-    //ф-я для подключния файрбейс при старте приложения
-    initFirebase();
-    checkEmptyTodoList();
+    checkEmptyTodo();
   }
 
-  void _menuOpen() {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (BuildContext context){
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Menu'),
-          ),
-          body: Row(
-            children: [
-              ElevatedButton(
-                  onPressed: (){
-                    Navigator.pop(context);
-                    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-                  },
-                  child: const Text('На главную')
-              ),
-              const Padding(padding: EdgeInsets.only(right: 15)),
-              const Text('Наше простое меню'),
-            ],
-          ),
-        );
-      })
-    );
+  ////////////-------------отдельная менюха---------////////////
+  // void _menuOpen() {
+  //   Navigator.of(context).push(
+  //     MaterialPageRoute(builder: (BuildContext context){
+  //       return Scaffold(
+  //         appBar: AppBar(
+  //           title: const Text('Menu'),
+  //         ),
+  //         body: Row(
+  //           children: [
+  //             ElevatedButton(
+  //                 onPressed: (){
+  //                   Navigator.pop(context);
+  //                   Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+  //                 },
+  //                 child: const Text('На главную')
+  //             ),
+  //             const Padding(padding: EdgeInsets.only(right: 15)),
+  //             const Text('Наше простое меню'),
+  //           ],
+  //         ),
+  //       );
+  //     })
+  //   );
+  // }
+
+  void checkEmptyTodo() {
+    FirebaseFirestore.instance.collection('items').get().then((snap) {
+      if (snap.docs.isNotEmpty) {
+        setState(() {
+          _emptyInfo = '';
+        });
+      } else {
+        setState(() {
+          _emptyInfo = 'Тут пока ничего нет';
+        });
+      }
+    });
   }
 
   @override
@@ -85,14 +75,47 @@ class _HomeState extends State<Home> {
         backgroundColor: Theme.of(context).colorScheme.primary,
         actions: [
           IconButton(
+            //кнопка для полного удаления
               onPressed: (){
-                //по задумке полностью очистить список
+
+                FirebaseFirestore.instance.collection('items').get().then((snap) {
+                  if (snap.docs.isNotEmpty) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context){
+                        return AlertDialog(
+                          title: const Text('Удалить все записи?'),
+                          actions: [
+                            ElevatedButton(
+                                onPressed: (){
+                                  //получаем снапшот
+                                  FirebaseFirestore.instance.collection('items').get().then((snap) {
+                                    //для каждого документа в снапшоте
+                                    for(DocumentSnapshot ds in snap.docs) {
+                                      //удалить
+                                      ds.reference.delete();
+                                    }
+                                  });
+                                  checkEmptyTodo();
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('Да')
+                            ),
+                            const Padding(padding: EdgeInsets.only(right: 10)),
+                            ElevatedButton(
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('Нет')
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                });
               },
-              icon: const Icon(Icons.delete_outline)
-          ),
-          IconButton(
-              onPressed: _menuOpen,
-              icon: const  Icon(Icons.menu_outlined)
+              icon: const Icon(Icons.delete_forever_sharp)
           )
         ],
       ),
@@ -116,68 +139,108 @@ class _HomeState extends State<Home> {
                   //если наш снепшот не имеет записей
                   if(!snapshot.hasData) {
                     //вернуть текст - нет записей
-                    return const Text('No strokes');
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                            Text(
+                              'Список загружается...',
+                              style: TextStyle(
+                                color: Colors.white10,
+                                fontSize: 16,
+                              ),
+                            ),
+                        ],
+                      ),
+                    );
                   } else {
                     //иначе вернуть ЛистВью.билдер со списком
-                    return ListView.builder(
-
-                      ////////////////////
-                      // остановился тут//
-                      ////////////////////
-
-                      itemCount: snapshot.data?.docs.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Dismissible( //свайповый блок
-                          key: Key(todoList[index]),  //что передаем
-                          child: Card(  //что внутри блока
-                            child: ListTile(
-                                title: Text(todoList[index]),
-                                trailing: IconButton(
-                                  onPressed: (){
-                                    deleteTodo(index);
+                    return Container(
+                      padding: const EdgeInsets.all(10),
+                      child: ListView.builder(
+                        itemCount: snapshot.data?.docs.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Dismissible( //свайповый блок
+                            key: Key(snapshot.data!.docs[index].id),  //что передаем
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                boxShadow: <BoxShadow>[
+                                  BoxShadow(
+                                    color: Colors.black54,
+                                    blurRadius: 20,
+                                  )
+                                ]
+                              ),
+                              child: Card( //что внутри блока
+                                child: ListTile(
+                                  onTap: (){
+                                    setState(() {
+                                      if(_overText==TextOverflow.ellipsis) {
+                                        _overText = TextOverflow.visible;
+                                      } else {
+                                        _overText = TextOverflow.ellipsis;
+                                      }
+                                    });
                                   },
-                                  icon: const Icon(Icons.delete),
-                                  color: Theme.of(context).colorScheme.primary,
-                                  tooltip: 'Удалить',
-                                )
+                                  title: Text(
+                                    snapshot.data!.docs[index].get('item'),
+                                    overflow: _overText,
+                                  ),
+                                  trailing: IconButton(
+                                    onPressed: (){
+                                      FirebaseFirestore.instance.collection('items').doc(snapshot.data!.docs[index].id).delete();
+                                      checkEmptyTodo();
+                                    },
+                                    icon: const Icon(Icons.delete),
+                                    color: Theme.of(context).colorScheme.primary,
+                                    tooltip: 'Удалить',
+                                  ),
+                                ),
+                              ),
                             ),
-                          ),
-                          onDismissed: (direction) {
-                            //if(direction==DismissDirection.startToEnd)
-                            deleteTodo(index);
-                          },
-                        );
-                      },
+                            onDismissed: (direction) {
+                              FirebaseFirestore.instance.collection('items').doc(snapshot.data!.docs[index].id).delete();
+                              checkEmptyTodo();
+                            },
+                          );
+                        },
+                      ),
                     );
                   }
                 }
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        alignment: Alignment.center,
-                        width: 150,
-                        height: 50,
-                        color: Colors.red,
-                        child: Text(_debugVar)
-                      ),
-                    ],
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(left: 2),
-                    child: IconButton(
-                      color: Colors.red,
-                      onPressed: (){},
-                      icon: const Icon(Icons.android_outlined)
-                    )
-                  )
-                ],
-              ),
+
+              //--------Админ панель--------//
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.start,
+              //   crossAxisAlignment: CrossAxisAlignment.end,
+              //   children: [
+              //     Column(
+              //       mainAxisAlignment: MainAxisAlignment.end,
+              //       children: [
+              //         Container(
+              //           padding: const EdgeInsets.all(5),
+              //           alignment: Alignment.center,
+              //           width: 200,
+              //           height: 200,
+              //           color: Colors.red,
+              //           child: Text('$_debugVar')
+              //         ),
+              //       ],
+              //     ),
+              //     Container(
+              //       margin: const EdgeInsets.only(left: 2),
+              //       child: IconButton(
+              //         color: Colors.red,
+              //         onPressed: (){},
+              //         icon: const Icon(Icons.android_outlined)
+              //       )
+              //     )
+              //   ],
+              // ),
+              //--------Админ панель--------/////
+
             ],
           ),
       floatingActionButton: FloatingActionButton(
@@ -190,38 +253,40 @@ class _HomeState extends State<Home> {
                 return AlertDialog(
                   title: const Text('Добавить задачу'),
                   content: TextField(
+                    minLines: 1,
+                    maxLines: 10,
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.black12,
+                    ),
                     onChanged: (String value) {
                       _userTodo = value;
                     },
                   ),
                   actions: [
-                    ElevatedButton(
+                    TextButton(
                         onPressed: (){
-
-                          //---------------------старый вариант
-                         /* if(_userTodo=='') {
-                            //нихуя не делать
-                          } else {
-                            setState(() {
-                              todoList.add(_userTodo);
-                            });
-                            _userTodo = '';
-                            Navigator.pop(context);
-                          }
-                          checkEmptyTodoList();*/
-
-                          //-----------------------новый вариант
-                          //Класс ФайрейсФайрстор(подключаемый выше) - инстанс(использовать текущее подключение к базе) -
-                          //коллекшен(коллекция с именем айтемс(если такой не будет, она автоматом создастся)) -
-                          //метод адд(добавить)
-                          //обавляем объект(ключ-значение). В значение записываем строку от пользователя
-                          FirebaseFirestore.instance
-                              .collection('items').add({'item': _userTodo});
-
                           Navigator.pop(context);
                         },
+                        child: const Text('Отмена')
+                    ),
+                    ElevatedButton(
+                        onPressed: (){
+                          if(_userTodo.isNotEmpty) {
+                            //Класс ФайрейсФайрстор(подключаемый выше) - инстанс(использовать текущее подключение к базе) -
+                            //коллекшен(коллекция с именем айтемс(если такой не будет, она автоматом создастся)) -
+                            //метод адд(добавить)
+                            //обавляем объект(ключ-значение). В значение записываем строку от пользователя
+                            FirebaseFirestore.instance
+                                .collection('items').add({'item': _userTodo});
+                            _userTodo = '';
+                            checkEmptyTodo();
+                            Navigator.pop(context);
+                          }
+                        },
                         child: const Text('Добавить')
-                    )
+                    ),
                   ],
                 );
               }
